936aa3b95b73611b6b2e240d96f01ae8a0c79c7b
Bereket Godebo <bereketgodebo@users.noreply.github.com>
2021-08-14 19:20:03 +0300

    wk04 hw complete

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -9,13 +9,15 @@ module Week04.Homework where
 
 import Data.Aeson            (FromJSON, ToJSON)
 import Data.Functor          (void)
-import Data.Text             (Text)
+import Data.Text             (Text, unpack)
 import GHC.Generics          (Generic)
 import Ledger
 import Ledger.Ada            as Ada
 import Ledger.Constraints    as Constraints
 import Plutus.Contract       as Contract
 import Plutus.Trace.Emulator as Emulator
+import Wallet.Emulator.Wallet
+
 
 data PayParams = PayParams
     { ppRecipient :: PubKeyHash
@@ -28,14 +30,26 @@ payContract :: Contract () PaySchema Text ()
 payContract = do
     pp <- endpoint @"pay"
     let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
-    void $ submitTx tx
-    payContract
+    Contract.handleError (\err -> Contract.logError $ "caught error " ++ unpack err) $ void $ submitTx tx
+    --payContract
+ 
 
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
 -- after each endpoint call.
 payTrace :: Integer -> Integer -> EmulatorTrace ()
-payTrace _ _ = undefined -- IMPLEMENT ME!
+payTrace value1 value2 = do
+            handle <- activateContractWallet (Wallet 1) payContract
+            let w2pkh = pubKeyHash $ walletPubKey $ Wallet 2
+            callEndpoint @"pay" handle $ PayParams { ppRecipient = w2pkh
+                                                   , ppLovelace  = value1
+                                                   }
+            void $ Emulator.waitNSlots 1
+            callEndpoint @"pay" handle $ PayParams { ppRecipient = w2pkh
+                                                   , ppLovelace  = value2
+                                                   }
+            void $ Emulator.waitNSlots 1
+
 
 payTest1 :: IO ()
 payTest1 = runEmulatorTraceIO $ payTrace 1000000 2000000
